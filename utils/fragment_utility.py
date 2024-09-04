6b3ee5341f900591207d67c9a181c8638f830062
Zheqi Jin <rv22218@bristol.ac.uk>
2024-09-04 13:21:58 +0100

    Zheqi - add fragment mode branch during initialization

---
+++ utils/fragment_utility.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def get_reduced_H(H_dict):
+    reduce_dict = {}
+    for key in H_dict.keys():
+        if len(H_dict[key]) > 1:
+            reduce_dict[H_dict[key][0]] = H_dict[key]
+    return reduce_dict
+
+def get_reduced_H_match():
+    
+
+def binary_matrix_to_list(mat):
+    pass
+
+def binary_matrix_to_index(mat):
+    return np.transpose(np.nonzero(mat))
