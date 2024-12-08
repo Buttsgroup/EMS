39253385a1fbd14ea8bebad23ee5a30fa254de73
Zheqi Jin <rv22218@bristol.ac.uk>
2024-12-08 00:24:46 +0000

    zheqi - combine test_EMS_sdf.py with test_EMS_xyz.py together into test_EMS_class.py

---
+++ tests/test_EMS_class.py
@@ -0,0 +1,57 @@
+import sys
+import os
+sys.path.append('/user/home/rv22218/work/inv_IMPRESSION/EMS')
+import EMS as ems
+import rdkit
+import pytest
+
+mol_list = ['testmol_1', 'testmol_xyz']
+
+class TestEMSclass:
+    def setup_method(self, method):
+        print()
+        print(f'Setting up {method}')
+
+    def teardown_method(self, method):
+        print()
+        print(f'Tearing down {method}')
+
+    @pytest.mark.parametrize('mol', mol_list)
+    def test_filenames(self, mol, request):
+        mol = request.getfixturevalue(mol)
+        Xfile = mol.file
+        Xfilename = Xfile.split('/')[-1]
+        Xstringfile = Xfile
+        print(f'EMS.file: {mol.file}')
+        print(f'EMS.id: {mol.id}')
+        print(f'EMS.filename: {mol.filename}')
+        print(f'EMS.stringfile: {mol.stringfile}')
+        
+        assert mol.file == Xfile
+        assert mol.filename == Xfilename
+        assert mol.stringfile == Xstringfile
+        assert mol.stringfile == mol.file
+
+
+    # def test_check_Zcoords_zero(self, testmol_1):
+    #     print(f'EMS.check_z_ords: {testmol_1.check_Zcoords_zero()}')
+    #     assert testmol_1.check_Zcoords_zero() == False
+
+    # def test_rdmol_type(self, testmol_1):
+    #     print(f'RDMol type: {type(testmol_1.rdmol)}')
+    #     assert type(testmol_1.rdmol) == rdkit.Chem.rdchem.Mol
+
+    # def test_RDMolProp(self, testmol_1):
+    #     print(f'RDMol.GetProp: {testmol_1.rdmol.GetProp("_Name")}')
+    #     if testmol_1.rdmol.GetProp('_Name') == testmol_1.id:
+    #         print('The _Name property of this RDMol is the same as its EMS.id')
+    #     assert True
+    
+
+    
+
+
+
+
+
+
